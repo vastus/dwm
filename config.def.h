847a2b9e48490e5127e6cf3703f600dc3c1d25f1
juho H <juhoth@ovi.com>
2013-07-11 07:24:06 +0300

    ff is floating by default

--- config.def.h
+++ config.def.h
@@ -21,7 +21,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "MPlayer",  NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       0,       False,       -1 },
+	{ "Firefox",  NULL,       NULL,       0,            True,       -1 },
 };
 
 /* layout(s) */
